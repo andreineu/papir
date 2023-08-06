3b8cb95403d08dd879a30010c32baed6471efa11
andreineu <andneu@yandex.ru>
2023-08-06 20:34:28 +0300

    refactor: extract note-list to components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,77 +1,9 @@
-import { useEffect, useState } from 'react';
-
-import { FilePlus2 } from 'lucide-react';
-
-import { api } from '@src/lib/api';
-import { useNotesStore } from '@src/lib/notes.store';
-import { Button } from '@src/ui-kit';
-
-import { NotePreview } from './note-preview';
+import { NoteList } from './note-list';
 
 export const Sidebar = () => {
   return (
     <div className="fixed inset-y-0 left-0 w-48 bg-neutral-900 p-2">
       <NoteList />
     </div>
-  );
-};
-
-const useHydratedNoteList = () => {
-  const { data: notes } = api.note.getAll.useQuery();
-  const [hydrated, setHydrated] = useState(false);
-
-  useEffect(() => {
-    if (!hydrated && notes) {
-      useNotesStore.setState({ notes });
-      setHydrated(true);
-    }
-  }, [hydrated, notes]);
-  const store = useNotesStore();
-  return store;
-};
-
-export const NoteList = () => {
-  const store = useHydratedNoteList();
-
-  return (
-    <>
-      <div className="flex justify-center p-4">
-        <Button
-          size="icon"
-          variant="ghost"
-          onClick={() => {
-            store.addNote({
-              title: 'Untitled',
-              content: '',
-            });
-          }}
-        >
-          <FilePlus2 />
-        </Button>
-      </div>
-      <div className="flex flex-col gap-1">
-        {store.notes?.map((note) => (
-          <NotePreview
-            key={note.id}
-            note={note}
-            onCopyNote={() => {
-              store.addNote({
-                title: note.title + ' copy',
-                content: note.content,
-              });
-            }}
-            onDeleteNote={() => {
-              store.deleteNote({ id: note.id });
-            }}
-            onRename={(title) => {
-              store.updateNote({ id: note.id, title });
-            }}
-            onStar={() => {
-              store.updateNote({ id: note.id, starred: !note.starred });
-            }}
-          />
-        ))}
-      </div>
-    </>
   );
 };
---
+++ src/components/note-list/note-list.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+
+import { FilePlus2 } from 'lucide-react';
+
+import { api } from '@src/lib/api';
+import { useNotesStore } from '@src/lib/notes.store';
+import { Button } from '@src/ui-kit';
+
+import { NotePreview } from '../note-preview';
+
+const useHydratedNoteList = () => {
+  const { data: notes } = api.note.getAll.useQuery();
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    if (!hydrated && notes) {
+      useNotesStore.setState({ notes });
+      setHydrated(true);
+    }
+  }, [hydrated, notes]);
+  const store = useNotesStore();
+  return store;
+};
+
+export const NoteList = () => {
+  const store = useHydratedNoteList();
+
+  return (
+    <>
+      <div className="flex justify-center p-4">
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() => {
+            store.addNote({
+              title: 'Untitled',
+              content: '',
+            });
+          }}
+        >
+          <FilePlus2 />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-1">
+        {store.notes?.map((note) => (
+          <NotePreview
+            key={note.id}
+            note={note}
+            onCopyNote={() => {
+              store.addNote({
+                title: note.title + ' copy',
+                content: note.content,
+              });
+            }}
+            onDeleteNote={() => {
+              store.deleteNote({ id: note.id });
+            }}
+            onRename={(title) => {
+              store.updateNote({ id: note.id, title });
+            }}
+            onStar={() => {
+              store.updateNote({ id: note.id, starred: !note.starred });
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
