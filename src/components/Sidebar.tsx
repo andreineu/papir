817af92717a26937ed1a835ce22ca28c877d352c
andreineu <andneu@yandex.ru>
2023-08-06 20:31:24 +0300

    feat: add note create button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
 
+import { FilePlus2 } from 'lucide-react';
+
 import { api } from '@src/lib/api';
 import { useNotesStore } from '@src/lib/notes.store';
+import { Button } from '@src/ui-kit';
 
 import { NotePreview } from './note-preview';
 
 export const Sidebar = () => {
   return (
-    <div className="fixed inset-y-0 left-0 bg-neutral-900 p-4">
+    <div className="fixed inset-y-0 left-0 w-48 bg-neutral-900 p-2">
       <NoteList />
     </div>
   );
@@ -31,28 +34,44 @@ export const NoteList = () => {
   const store = useHydratedNoteList();
 
   return (
-    <div className="fixed inset-y-0 left-0 bg-neutral-900 p-4">
-      {store.notes?.map((note) => (
-        <NotePreview
-          key={note.id}
-          note={note}
-          onCopyNote={() => {
+    <>
+      <div className="flex justify-center p-4">
+        <Button
+          size="icon"
+          variant="ghost"
+          onClick={() => {
             store.addNote({
-              title: note.title + ' copy',
-              content: note.content,
+              title: 'Untitled',
+              content: '',
             });
           }}
-          onDeleteNote={() => {
-            store.deleteNote({ id: note.id });
-          }}
-          onRename={(title) => {
-            store.updateNote({ id: note.id, title });
-          }}
-          onStar={() => {
-            store.updateNote({ id: note.id, starred: !note.starred });
-          }}
-        />
-      ))}
-    </div>
+        >
+          <FilePlus2 />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-1">
+        {store.notes?.map((note) => (
+          <NotePreview
+            key={note.id}
+            note={note}
+            onCopyNote={() => {
+              store.addNote({
+                title: note.title + ' copy',
+                content: note.content,
+              });
+            }}
+            onDeleteNote={() => {
+              store.deleteNote({ id: note.id });
+            }}
+            onRename={(title) => {
+              store.updateNote({ id: note.id, title });
+            }}
+            onStar={() => {
+              store.updateNote({ id: note.id, starred: !note.starred });
+            }}
+          />
+        ))}
+      </div>
+    </>
   );
 };
