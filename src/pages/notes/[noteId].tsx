e3ce53b77014d308adcffb1d167bcef96ea9c7d3
andreineu <andneu@yandex.ru>
2023-08-08 22:24:31 +0300

    fix: broken build

--- src/pages/notes/[noteId].tsx
+++ src/pages/notes/[noteId].tsx
@@ -1,11 +1,48 @@
 import { type PropsWithChildren, useEffect, useState } from 'react';
 
+import { type Editor as EditorType } from '@tiptap/core';
 import { FileX } from 'lucide-react';
+import { useRouter } from 'next/router';
 
 import { Editor } from '@src/components/editor';
+import { api } from '@src/lib/api';
+import { useNoteUpdateMutation } from '@src/lib/hooks/note.hooks';
+import { useDebouncedCallback } from '@src/lib/use-debounced-callback';
 import { useToast } from '@src/ui-kit';
 
-import { useNote } from './lib';
+const useNote = () => {
+  const router = useRouter();
+  const id = router.query.noteId as string;
+
+  const { data } = api.note.getById.useQuery({
+    id,
+  });
+
+  const { mutate } = useNoteUpdateMutation();
+
+  const updateContent = ({ editor }: { editor: EditorType }) => {
+    mutate({
+      id,
+      content: editor.getHTML(),
+    });
+  };
+
+  const updateTitle = (title: string) => {
+    mutate({
+      id,
+      title,
+    });
+  };
+
+  const handleUpdateContent = useDebouncedCallback(updateContent, 500);
+  const handleUpdateTitle = useDebouncedCallback(updateTitle, 500);
+
+  return {
+    data,
+    handleUpdateContent,
+    handleUpdateTitle,
+  };
+};
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
