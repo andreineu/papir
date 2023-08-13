7ec9935f3bdcc245cc962d52cb24d1b668d5c053
andreineu <andneu@yandex.ru>
2023-08-13 12:14:36 +0300

    test: add tests for button component

--- src/ui-kit/button/button.tsx
+++ src/ui-kit/button/button.tsx
@@ -47,6 +47,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
+        data-testid="button"
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
