13747da7c5fc92b25b845b1f5a51ca55fde52dd6
andreineu <andneu@yandex.ru>
2023-08-14 14:27:26 +0300

    feat: add note delete alert dialog

--- src/components/note-preview-menu/note-preview-menu.tsx
+++ src/components/note-preview-menu/note-preview-menu.tsx
@@ -1,22 +1,35 @@
-import { type FC } from 'react';
+import { type FC, useState } from 'react';
 
-import { NotePreviewContextMenuContent } from './note-preview-context-menu';
-import { NotePreviewDropdownMenuContent } from './note-preview-dropdown-menu';
+import { AlertDialog } from '@src/ui-kit';
+
+import { NotePreviewAlertDialog } from './ui/note-preview-alert-dialog';
+import { NotePreviewContextMenuContent } from './ui/note-preview-context-menu';
+import { NotePreviewDropdownMenuContent } from './ui/note-preview-dropdown-menu';
 
 interface NotePreviewMenuProps {
-  onRenameClick?: () => void;
-  onCopyClick?: () => void;
-  onStarClick?: () => void;
-  onDeleteClick?: () => void;
+  onRenameClick: () => void;
+  onCopyClick: () => void;
+  onStarClick: () => void;
+  onDeleteClick: () => void;
 
   isStarred?: boolean;
 }
 
 export const NotePreviewMenu: FC<NotePreviewMenuProps> = (props) => {
+  const [isAlertDialogOpen, setIsAlertDialogOpen] = useState(false);
   return (
     <>
-      <NotePreviewContextMenuContent {...props} />
-      <NotePreviewDropdownMenuContent {...props} />
+      <NotePreviewContextMenuContent
+        {...props}
+        onDeleteClick={() => setIsAlertDialogOpen(true)}
+      />
+      <NotePreviewDropdownMenuContent
+        {...props}
+        onDeleteClick={() => setIsAlertDialogOpen(true)}
+      />
+      <AlertDialog open={isAlertDialogOpen} onOpenChange={setIsAlertDialogOpen}>
+        <NotePreviewAlertDialog onDeleteConfirm={props.onDeleteClick} />
+      </AlertDialog>
     </>
   );
 };
