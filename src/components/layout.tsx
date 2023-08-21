38f0b75522343b3a2742f8e2af75f65205f1505e
andreineu <andneu@yandex.ru>
2023-08-21 18:07:09 +0300

    feat: redirect from /notes to /

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,11 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Sidebar />
-      <main className="ml-48 min-h-screen">{children}</main>
+      <main className="ml-48 min-h-screen">
+        <div className="mx-auto flex h-screen flex-col items-center gap-4 pt-16 md:max-w-[576px] lg:max-w-[768px] xl:max-w-[1024px]">
+          {children}
+        </div>
+      </main>
     </>
   );
 };
